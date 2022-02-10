75a7f2a5400e1cfe7578639fce36b70818a82702
mohamedelmnegged <mohamedaahmed92@gmail.com>
2022-02-10 10:02:41 +0200

    Add cart pages and its route with Ajax route for adding product to cart

--- Shop/Controllers/User/CartController.cs
+++ Shop/Controllers/User/CartController.cs
@@ -3,8 +3,10 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Shop.Data;
 using Shop.Data.Tables;
+using Shop.ModelViews;
 using System;
 using System.Collections.Generic;
+using System.DirectoryServices.Protocols;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -21,7 +23,30 @@ namespace Shop.Controllers.User
         {
             this.userManager = userManager;
             this.context = context;
+        }  
+        public async Task<IActionResult> Index()
+        {
+          var model =   (from order in context.Orders.Where(a => a.User.UserName == User.Identity.Name)
+             join cart in context.Carts on order.Id equals cart.OrderId
+             join product in context.Products on order.ProductId equals product.Id
+             select new CartView
+             {
+                 Image = product.Image,
+                 Amount = order.Amount,
+                 Name = product.Name,
+                 Description = product.Description,
+                 Price = product.Price 
+             });
+            return View("../User/Cart", model);
         }
+        public record dd();
+        [HttpPost] 
+        public async Task<IActionResult> UpdateCounter(string[] counter)
+        {
+          //  var data = HttpContext.Response.Headers; 
+            return Json(counter);
+        }
+        [HttpPost]
         public async Task<IActionResult> Add(int Id)
         {
             if(Id != null)
@@ -30,11 +55,20 @@ namespace Shop.Controllers.User
                 if(user != null)
                 {
                     var checkOrder = context.Orders.Where(a => a.ProductId == Id && a.UserId == user.Id);
-                   
+                    var count = context.Orders.Where(a => a.User.UserName == User.Identity.Name)
+                        .Join(context.Carts, order => order.Id, cart => cart.OrderId,
+                                    (order, cart) => new
+                                    {
+                                        order = order.Id
+                                    }
+                                    ).Count();
+
                     if (checkOrder.Count() > 0)
                     {
                         checkOrder.FirstOrDefault().Amount += 1;
                         await context.SaveChangesAsync();
+                        
+                        return Json(new { Count = count, msg = "Already exsit in Cart and increased Amount" });
                     }
                     else
                     {
@@ -55,12 +89,11 @@ namespace Shop.Controllers.User
                         await context.Carts.AddAsync(cart);
                         await context.SaveChangesAsync();
 
+                        return Json(new {Count = count + 1, msg = "Added To Cart Successfuly" });
                     }
-
-                    return Json("Done"); 
                 }
             }
-            return View(); 
+            return Json("The Id not exsit"); 
         }
     }
 }
--- Shop/wwwroot/js/site.js
+++ Shop/wwwroot/js/site.js
@@ -2,3 +2,25 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//asp - area="user" asp - controller="cart"  asp - action="add" asp - route - id="@item.Id"
+
+function AddToCart(id) {
+    console.log(id)
+    $.ajax({
+        type: 'POST',
+        url: '/Cart/Add/' + id,
+       // contentType: 'application/x-www-form-urlencoded; charset=UTF-8', // when we use .serialize() this generates the data in query string format. this needs the default contentType (default content type is: contentType: 'application/x-www-form-urlencoded; charset=UTF-8') so it is optional, you can remove it
+       // data: data,
+        success: function (result) {
+            alert(result["msg"]);
+            document.getElementById("cartCount").innerHTML = result["count"];
+            console.log(result);
+        },
+        error: function () {
+            alert('You should Login First');
+            console.log('Failed ');
+        }
+    })
+}
+
